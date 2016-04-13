780318b1c073ad2883ed7e2b9adafb0eca972f0a
packy <lpreterite@126.com>
2016-04-13 18:10:33 +0800

    修复初次编辑时光标重置问题

--- src/vue-tinymce.js
+++ src/vue-tinymce.js
@@ -16,27 +16,32 @@ module.exports = {
     ready: function(){
         var self = this
         var config = require('tinymce.config')
+
+        var unwatch = this.$watch('model', function(val, oldVal){
+            if(!!tinymce.activeEditor) tinymce.activeEditor.setContent(val)
+        })
+
         config.target = this.$els.input
         config.style_formats = require('tinymce.config.style_formats')
         config.setup = function(editor){
             //init
             editor.on('init', function(){
+                var change = function(){
+                    self.model = editor.getContent()
+                }
                 editor.setContent(self.model)
-                editor.on('change', function(){
-                    if(!self.vueChange) self.model = tinymce.activeEditor.getContent()
+                editor.on('change', function(){//undo,redo
+                    //由于第一次加载存在刷新机制，需判断是否为
+                    if(self.vueChange || editor.undoManager.data.length<=1) return unwatch();
+                    change()
                 })
+                editor.on('undo', change)
+                editor.on('redo', change)
             })
         }
         tinymce.init(config)
     },
     beforeDestroy: function(){
         tinymce.remove()
-    },
-    watch: {
-        'model': function(val, oldVal){
-            this.vueChange = true
-            if(!!tinymce.activeEditor) tinymce.activeEditor.setContent(val)
-            this.vueChange = false
-        }
     }
 }
